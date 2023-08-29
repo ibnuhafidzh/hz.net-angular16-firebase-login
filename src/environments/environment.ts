ce471e465afdd05d68da9e610ac3cb86fbdc1ace
ibnu.hz <ibnu.hz@outlook.com>
2023-08-29 14:50:00 +0700

    first upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'mycv-b912f',
+    appId: '1:394682975466:web:24f128c696263127874393',
+    databaseURL: 'https://mycv-b912f-default-rtdb.firebaseio.com',
+    storageBucket: 'mycv-b912f.appspot.com',
+    apiKey: 'AIzaSyCYoMMP0VGNaKHOiBaIdRDNKLWmp9lSOkM',
+    authDomain: 'mycv-b912f.firebaseapp.com',
+    messagingSenderId: '394682975466',
+    measurementId: 'G-JR8DSCVJC7',
+  },
+};
